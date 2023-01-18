94d5fcffad0777d8cb74683334e383d48a526748
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-01-18 09:26:15 +0100

    image contact

--- _pages/summary.php
+++ _pages/summary.php
@@ -18,7 +18,7 @@ if ($arraycheck < 11) {
             <div class="col-lg-4">
                 <div class="card mb-4">
                     <div class="card-body text-center">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                        <img src="/img/user.png"
                             alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
                         <h5 class="my-3">
                             <?php echo $_SESSION['name']; ?>
